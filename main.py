a8ea7da16e6680a6a70336cd4360b3cfd4617e35
vinithmoduguru <54624130+vinithmoduguru@users.noreply.github.com>
2023-12-18 02:21:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import os
 
 app = Flask(__name__)
 
+dummy_data = [
+    {'id': 1, 'name': "A", 'composition': [30, 20, 20, 10, 20]},
+    {'id': 2, 'name': "B", 'composition': [40, 10, 15, 10, 25]},
+    {'id': 3, 'name': "C", 'composition': [35, 25, 10, 10, 20]},
+    {'id': 4, 'name': "D", 'composition': [30, 25, 20, 10, 15]},
+]
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+@app.route('/', methods=['GET'])
+def get_data():
+    response = jsonify(dummy_data)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
